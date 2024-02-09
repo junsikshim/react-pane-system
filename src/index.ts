0ad28b260c618e8f51629714b5eb649b46dd5b8f
Junsik Shim <junsik.shim@gmail.com>
2024-02-09 14:58:32 +0900

    Change the exports.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { default as PaneSystem } from './PaneSystem';
 export { default as PaneRow } from './PaneRow';
 export { default as Pane } from './Pane';
-export { default as RowSplitter } from './RowSplitter';
-export { default as ColumnSplitter } from './ColumnSplitter';
